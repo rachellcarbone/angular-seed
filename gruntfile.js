4c360f81dc8b5a5c87b7e65b235b2b948fb6f44a
Rachel L Carbone <rachellcarbone@gmail.com>
2015-11-11 12:11:23 -0500

    Added gruntfile.js
    Grunt uses Bower and Composer to pull project dependencies.
    
    Grunt uses watch to keep the Node Static server running and to also
    automatically compile Less files when they are saved during development.
    
    Jit Grunt is used to load Grunt dependencies on the fly.

---
+++ gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function (grunt) {
+    require('jit-grunt')(grunt)({
+        pluginsRoot: 'node_modules'
+    });
+
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: false,
+                    yuicompress: false,
+                    optimization: 2
+                },
+                files: {
+                    "www/styles/styles.css": "www/styles/less/main.less" // destination file and source file
+                }
+            }
+        },
+        lessmin: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "www/styles/styles.min.css": "www/styles/less/main.less" // destination file and source file
+                }
+            }
+        },
+        nodestatic: {
+            server: {
+                options: {
+                    dev: true,
+                    verbose: false,
+                    port: 8888,
+                    base: '.'
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['www/styles/less/**/*.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        },
+        composer: {
+            options: {
+                usePhp: false,
+                phpArgs: {},
+                flags: [],
+                cwd: 'api/v1',
+                composerLocation: '/usr/bin/composer'
+            },
+            some_target: {}
+        },
+        bower: {
+            install: {
+                options: {
+                    targetDir: 'www/bower_components',
+                    layout: 'byType',
+                    install: true,
+                    verbose: false,
+                    cleanTargetDir: false,
+                    cleanBowerDir: false,
+                    bowerOptions: {}
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('init', ['composer', 'bower', 'less', 'lessmin']);
+    grunt.registerTask('default', ['less', 'lessmin', 'nodestatic', 'watch']);
+    grunt.registerTask('styles', ['less', 'watch']);
+};
