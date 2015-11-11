11ae62ab66bb80c5e0f45d5f9781d98fc5b56f69
Rachel L Carbone <rachellcarbone@gmail.com>
2015-11-11 15:18:34 -0500

    Enable Angular
    Include all configuration modules into theApp and include all
    dependencies into the index.html.

---
+++ www/app/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* 
+ * Angular Seed - v0.1 
+ * https://github.com/rachellcarbone/angular-seed/
+ * Copyright (c) 2015 Rachel L Carbone; Licensed MIT
+ */
+
+// Include modules, and run application.
+angular.module('theApp', [
+    'app.config',
+    'app.router',
+    'app.run.dev',
+    'app.run'
+]);
