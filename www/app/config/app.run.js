95db67db4ec10335e99263c29ca01d0fb59755db
Rachel L Carbone <rachellcarbone@gmail.com>
2015-11-11 15:15:05 -0500

    App run
    App run changes a root scope variable for the title of the page using
    state data (if it exists) every time the state is successfully changed.

---
+++ www/app/config/app.run.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/*
+ * Application .run
+ * 
+ * On a successful state change set the root scopes title property
+ * used for the html <title> tag.
+ * 
+ * Angular .run exicuted after the .config function.
+ */
+
+var app = angular.module('app.run', []);
+app.run(function ($rootScope) {
+    
+    // Execute every time the state is successfully changed (after interceptors)
+    $rootScope.$on('$stateChangeSuccess', function (event, current, previous) {
+        // If the new state has property for 'title' (set in the state declaration) 
+        // set the root scope variable title to be used by the index.html template.
+        $rootScope.title = (current.hasOwnProperty('title')) ? current.title : 'Rachel\'s New Project Seed';
+    });
+    
+});
