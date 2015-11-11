dbaa7b66580e621b2d3a8135610a32d87616ccad
Rachel L Carbone <rachellcarbone@gmail.com>
2015-11-11 15:16:41 -0500

    App run dev
    App run dev adds any development settings to the app.
    
    When the state begins to change if the template cache has a copy of the
    template we are going to view, delete it, so that it is always
    refreshed. Remove for live environment. Used to help clients view
    changes rapidly.

---
+++ www/app/config/app.run.dev.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/*
+ * Dev Application .run
+ * 
+ * On each state change start, delete the template for the new state from 
+ * angular template cache if it has been saved. This is done because testers
+ * may have trouble seeing changes quickly on dev servers.
+ * 
+ * Angular .run exicuted after the .config function.
+ * 
+ * IMPORTANT: Remove when out of dev mode
+ */
+
+var app = angular.module('app.run.dev', []);
+app.run(function ($rootScope, $templateCache) {
+    
+    // Execute every time a state change begins
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+        // If the state we are headed to has cached template views
+        if (typeof (toState) !== 'undefined' && typeof (toState.views) !== 'undefined') {
+            // Loop through each view in the cached state
+            for (var key in toState.views) {
+                // Delete templeate from cache
+                $templateCache.remove(toState.views[key].templateUrl);
+            }
+        }
+    });
+
+    
+});
