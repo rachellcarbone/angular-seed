9b4f1377016584edc6c3059cd7610b75235ad841
Rachel L Carbone <rachellcarbone@gmail.com>
2015-11-12 16:44:36 -0500

    Commented Maintenance Page

--- www/app/views/maintenance/maintenance.controller.js
+++ www/app/views/maintenance/maintenance.controller.js
@@ -1,7 +1,14 @@
 'use strict';
 
+/* 
+ * Maintenance Page
+ * 
+ * This is the maintenance page that visitors will be redirected to when 
+ * "maintenance mode" is turned on. It doesnt use any other views and is 
+ * loaded from the index.html.
+ */
+
 angular.module('app.maintenance', [])
-    .controller('MaintenanceCtrl', ['$scope',
-    function($scope) {
+    .controller('MaintenanceCtrl', ['$scope', function($scope) {
         
     }]);
