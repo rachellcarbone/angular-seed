f0d7e74bd0519f5815638fad797d19fd4db86e51
Rachel L Carbone <rachellcarbone@gmail.com>
2015-11-12 13:50:45 -0500

    Committing Adming Header and Footer
    I missed the admin header and footer in my previous commit.

---
+++ www/app/views/admin/adminFooter/adminFooter.controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* 
+ * Layout Footer: Admin
+ * 
+ * Used to control the admin layout footer.
+ */
+
+angular.module('app.admin.footer', [])
+        .controller('AdminFooterCtrl', ['$scope', function ($scope) {
+        
+    }]);
---
+++ www/app/views/admin/adminHeader/adminHeader.controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* 
+ * Layout Header: Admin
+ * 
+ * Used to control the admin layout header and top navigtion.
+ */
+
+angular.module('app.admin.header', [])
+        .controller('AdminHeaderCtrl', ['$scope', function ($scope) {
+        
+    }]);
