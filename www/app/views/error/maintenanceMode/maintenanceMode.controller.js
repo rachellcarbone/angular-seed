705e0742768b60326b7fda6bfbdbaef0694d77b9
Rachel L Carbone <rachellcarbone@gmail.com>
2015-11-12 13:50:03 -0500

    Maintenance Mode
    Put the maintenance mode into its own directory because it will not use
    a header or a footer from any other view.

--- www/app/views/error/maintenanceMode/maintenanceMode.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('pages.error.maintenance', [])
-    .controller('ErrorPagesCtrl', ['$scope',
-    function($scope) {
-        
-    }]);
---
+++ www/app/views/maintenance/maintenance.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('app.maintenance', [])
+    .controller('MaintenanceLayoutCtrl', ['$scope',
+    function($scope) {
+        
+    }]);
