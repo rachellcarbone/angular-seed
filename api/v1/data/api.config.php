58c5f3f9916e9a72a7cbb4d90fe949e836107641
Rachel L Carbone <rachellcarbone@gmail.com>
2015-11-13 16:36:38 -0500

    API Config file
    Added a php file to hold system config variables for the API. I kept it
    very simple because I want to replace this with a better system that
    will configure both sides of the application.

---
+++ api/v1/data/api.config.php
@@ -0,0 +1,41 @@
+<?php namespace API\Data;
+
+class APIConfig {
+
+    function __construct() {
+        $this->setAPIConfig(filter_input(INPUT_SERVER, 'SERVER_ADDR'));
+    }
+
+    function setAPIConfig($ip) {
+        $default = array(
+            'apiVersion' => 'v1',
+            'debugMode' => true,
+            'dbHost' => 'localhost',
+            'db' => 'angular_seed',
+            'dirPublic' => 'public/',
+            'dirSystem' => 'api/system/',
+            'dirLogs' => 'api/system/logs/',
+        );
+
+        switch ($ip) {
+            case '::1':
+            default:
+                // Localhost
+                $this->config = array_merge($default, array(
+                    'db' => 'angular_seed',
+                    'dbUser' => 'angular_seed',
+                    'dbPass' => 'angular_seed',
+                    'systemPath' => 'C:/xampp/htdocs/webdev/angular-seed/',
+                    'websiteUrl' => 'http://www.seed.dev/'
+                ));
+        }
+    }
+
+    function get($opt = false) {
+        if ($opt !== false && isset($this->config[$opt])) {
+            return $this->config[$opt];
+        }
+        return $this->config;
+    }
+
+}
