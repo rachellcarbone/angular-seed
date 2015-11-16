bbb72ac157db91a752e83c3e398adacfcf16c526
Rachel L Carbone <rachellcarbone@gmail.com>
2015-11-16 12:31:23 -0500

    Created add / edit / remove sql for User
    Created a sql statement for insert, update, select and deleting of the
    users.

--- api/v1/controllers/user/user.data.php
+++ api/v1/controllers/user/user.data.php
@@ -1,14 +1,29 @@
 <?php namespace API;
-require_once dirname(dirname(__FILE__)) . '/abstract.data.php';
+ require_once dirname(dirname(dirname(__FILE__))) . '/services/api.dbconn.php';
 
-class UserData extends PDOData {
-  
-    public function __construct() {
-        parent::__construct();
+class UserData {
+    
+    public static function selectUsers() {
+        return DBConn::select('SELECT * FROM users ORDER BY name_last ASC;');
     }
     
-    public function db_selectUserById($id) {
-        return $this->db->selectOne("SELECT id, name_first as nameFirst, name_last as nameLast, email, role_id as roleId "
-                . "FROM users WHERE id = " . $id . " LIMIT 1;");
+    public static function selectUserById($id) {
+        return DBConn::selectOne("SELECT id, name_first as nameFirst, name_last as nameLast, email, role_id as roleId "
+                . "FROM users WHERE id = :id LIMIT 1;", array('id' => $id));
+    }
+  
+    public static function insertUser($validUser) {
+        return DBConn::preparedQuery("INSERT INTO users(name_first, name_last, email, password, role_id, plan_id) "
+                . "VALUES (:name_first, :name_last, :email, :password, :role_id, :plan_id);", $validUser);
+    }
+    
+    public static function updateUser($validUser) {
+        return DBConn::preparedQuery("UPDATE users SET name_first=:name_first, name_last=:name_last, "
+                . "email=:email, password=:password, role_id=:role_id, plan_id=:plan_id "
+                . "WHERE id = :id;", $validUser);
+    }
+    
+    public static function deleteUser($id) {
+        return DBConn::preparedQuery("DELETE FROM users WHERE id = :id LIMIT 1;", array('id' => $id));
     }
 }
