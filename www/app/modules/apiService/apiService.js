0844f776a6ee3193ad5f051c3545fe697c65c504
Rachel L Carbone <rachellcarbone@gmail.com>
2016-01-13 12:51:35 -0500

    Allow cross domain ajax requests

--- www/app/modules/apiService/apiService.js
+++ www/app/modules/apiService/apiService.js
@@ -14,7 +14,7 @@ angular.module('api.v1', [
     var api = {};
     
     // API URL Base
-    api.apiUrl = '../api/v1/';
+    api.apiUrl = 'http://api.seed.dev/v1/';
     
     // Default Error Message 
     // Used when no other message is available.
