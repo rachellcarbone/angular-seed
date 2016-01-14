08314b3d9455f39f0cbea5e716a6f67bfa36ac2d
Rachel L Carbone <rachellcarbone@gmail.com>
2016-01-13 20:38:48 -0500

    Changes session names to variables
    To prevent errors by typos and so I could obscure the variable names in
    the browser cookie I changed the session and cookie names to be
    variables instead of strings.

--- api/v1/controllers/auth/auth.session.php
+++ api/v1/controllers/auth/auth.session.php
@@ -2,9 +2,13 @@
 require_once dirname(__FILE__) . '/auth.data.php';
 
 class AuthSession {
+    
+    private $cookieUserEmail = '_rcsessUE';
+    private $cookieAuthToken = '_rcsessAT';
+    private $sessionActiveUserId = '_rcsessAUI';
 
     private function setSession($username) {
-        $_SESSION['auth_active'] = $username;
+        $_SESSION[$this->sessionActiveUserId] = $username;
     }
     
     private function destroySession() {
@@ -13,8 +17,8 @@ class AuthSession {
     }
     
     private function getUserByEmailFromSession() {
-        if(isset($_SESSION['auth_active'])) {
-            $user = AuthData::selectUserByEmail($_SESSION['auth_active']);
+        if(isset($_SESSION[$this->sessionActiveUserId])) {
+            $user = AuthData::selectUserByEmail($_SESSION[$this->sessionActiveUserId]);
             unset($user['password']);
             return $user;
         } else {
@@ -25,26 +29,26 @@ class AuthSession {
     private function setRememberMeCookie($user) {
         $token = password_hash(uniqid(), PASSWORD_DEFAULT);
         if(AuthData::updateAuthToken($user['id'], $token)) {
-            setcookie('auth_token', $token, time() + (86400 * 7), '/'); // Seven Days
-            setcookie('email', $user['email'], time() + (86400 * 7), '/'); // Seven Days
+            setcookie($this->cookieAuthToken, $token, time() + (86400 * 7), '/'); // Seven Days
+            setcookie($this->cookieUserEmail, $user[$this->cookieUserEmail], time() + (86400 * 7), '/'); // Seven Days
         }
     }
     
     private function destroyCookie() {
-        AuthData::deleteAuthToken(filter_input(INPUT_COOKIE, 'auth_token'));
-        if (filter_input(INPUT_COOKIE, 'auth_token')) {
-            unset($_COOKIE['auth_token']);
-            setcookie('auth_token', null, -1, '/');
+        AuthData::deleteAuthToken(filter_input(INPUT_COOKIE, $this->cookieAuthToken));
+        if (filter_input(INPUT_COOKIE, $this->cookieAuthToken)) {
+            unset($_COOKIE[$this->cookieAuthToken]);
+            setcookie($this->cookieAuthToken, null, -1, '/');
         }
-        if (filter_input(INPUT_COOKIE, 'email')) {
-            unset($_COOKIE['email']);
-            setcookie('email', null, -1, '/');
+        if (filter_input(INPUT_COOKIE, $this->cookieUserEmail)) {
+            unset($_COOKIE[$this->cookieUserEmail]);
+            setcookie($this->cookieUserEmail, null, -1, '/');
         }
     }
     
     private function getUserByEmailFromCookie() {
-        if(filter_input(INPUT_COOKIE, 'auth_token')) {
-            return AuthData::selectUserByToken(filter_input(INPUT_COOKIE, 'auth_token'));
+        if(filter_input(INPUT_COOKIE, $this->cookieAuthToken)) {
+            return AuthData::selectUserByToken(filter_input(INPUT_COOKIE, $this->cookieAuthToken));
         } else {
             return false;
         }
