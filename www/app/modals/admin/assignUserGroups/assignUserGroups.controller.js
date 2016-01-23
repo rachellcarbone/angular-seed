d435846dad6cfabe1b2614711d4468fec2c49f1c
Rachel L Carbone <rachellcarbone@gmail.com>
2016-01-23 01:07:45 -0500

    Added a controller and template for admin modals.
    This is a template to be modified for the specific forms.

---
+++ www/app/modals/admin/assignUserGroups/assignUserGroups.controller.js
@@ -0,0 +1,80 @@
+'use strict';
+
+/* @author  Rachel Carbone */
+
+angular.module('app.modal.assignUserGroups', [])
+    .controller('AssignUserGroupsCtrl',
+    function($rootScope, $scope, $modalInstance, CategoryService, modalSettings, notifications, CommonMethods) {
+    $scope.restrictTo = "categoryModal";
+        
+    $scope.form = {};
+    $scope.category = (modalSettings.category) ? modalSettings.category : {};
+    $scope.categories = (modalSettings.categories) ? modalSettings.categories : [];
+    $scope.inNewMode = (typeof($scope.category.id) === 'undefined');
+    
+    $scope.buttonNew = function() {
+        var found = angular.copy(CommonMethods.findAllInArray($scope.categories, 'category', $scope.category.category));
+        
+        if(found.length > 0) {
+            $scope.category.category = '';
+            $rootScope.$broadcast('alertbar-add', { 'sender' : $scope.restrictTo, 'message': 'A category with that name already exists.', 'type' : 'danger' });
+        } else if ($scope.forms.categoryForm.$valid) {
+            
+            CategoryService.insertCategory($scope.category)
+                .then(function(result) {
+                    notifications.showSuccess(result);
+                    $modalInstance.close(true);
+                }, function(error) {
+                    $rootScope.$broadcast('alertbar-add', { 'sender' : $scope.restrictTo, 'message': error, 'type' : 'danger' });
+                    $scope.$broadcast('form-validate');
+                });
+                
+        } else {
+            $rootScope.$broadcast('alertbar-add', { 'sender' : $scope.restrictTo, 'message': 'Please fill in all required fields.', 'type' : 'danger' });
+            $scope.$broadcast('form-validate');
+        }
+    };
+    
+    $scope.buttonSave = function() {
+        var found = angular.copy(CommonMethods.findAllInArray($scope.categories, 'category', $scope.category.category));
+        
+        if(found.length > 1) {
+            $scope.category.category = '';
+            $rootScope.$broadcast('alertbar-add', { 'sender' : $scope.restrictTo, 'message': 'A category with that name already exists.', 'type' : 'danger' });
+        } else if ($scope.forms.categoryForm.$valid) {
+            
+            CategoryService.saveCategory($scope.category, $scope.category.id)
+                .then(function(result) {
+                    notifications.showSuccess(result);
+                    $modalInstance.close(true);
+                }, function(error) {
+                    $rootScope.$broadcast('alertbar-add', { 'sender' : $scope.restrictTo, 'message': error, 'type' : 'danger' });
+                    $scope.$broadcast('form-validate');
+                });
+                
+        } else {
+            $rootScope.$broadcast('alertbar-add', { 'sender' : $scope.restrictTo, 'message': 'Please fill in all required fields.', 'type' : 'danger' });
+            $scope.$broadcast('form-validate');
+        }
+    };
+    
+    $scope.buttonDelete = function() {
+        if (confirm('Are you sure you want to permanently delete this category? This action cannot be undone.')) {
+            
+            CategoryService.deleteCategory($scope.category.id)
+                .then(function(result) {
+                    notifications.showSuccess(result);
+                    $modalInstance.close(true);
+                }, function(error) {
+                    $rootScope.$broadcast('alertbar-add', { 'sender' : $scope.restrictTo, 'message': error, 'type' : 'danger' });
+                });
+                
+        } else {
+            $scope.modalInEditMode(false);
+        }
+    };
+        
+    $scope.buttonCancel = function() {
+        $modalInstance.dismiss(false);
+    };
+});
