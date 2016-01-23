0c62ede51391c0116f0a5b581d62f2dbec49d55d
Rachel L Carbone <rachellcarbone@gmail.com>
2016-01-23 18:50:01 -0500

    Added user profile form.

--- www/app/views/member/profile/profile.controller.js
+++ www/app/views/member/profile/profile.controller.js
@@ -7,7 +7,35 @@
  */
 
 angular.module('app.member.profile', [])
-    .controller('MemberProfileCtrl', ['$scope', function($scope) {
-
+    .controller('MemberProfileCtrl', ['$scope', '$log', 'UserSession', 'ApiRoutesUsers', 
+    function($scope, $log, UserSession, ApiRoutesUsers) {
         
+        /* Used to restrict alert bars */
+        $scope.restrictTo = "edit-user-modal";
+
+        /* Holds the add / edit form on the modal */
+        $scope.form = {};
+
+        /* User to display and edit */
+        $scope.user = UserSession.get();
+
+        /* Click event for the Add / New button */
+        $scope.buttonChangePassword = function() {
+            ApiRoutesUsers.changePassword($scope.user).then(
+                function (result) {
+                    $scope.editMode = false;
+                }, function (error) {
+                    $log.info(error);
+                });
+        };
+
+        /* Click event for the Save button */
+        $scope.buttonSave = function() {
+            ApiRoutesUsers.saveUser($scope.user).then(
+                function (result) {
+                    $scope.editMode = false;
+                }, function (error) {
+                    $log.info(error);
+                });
+        };
     }]);
