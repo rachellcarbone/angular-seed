56c3d84aad4afdce77220f788642d2c63727994e
Rachel L Carbone <rachellcarbone@gmail.com>
2016-01-25 12:23:28 -0500

    Starting signup page
    Adding facebook to signup page from the get go

--- www/app/views/auth/signup/signup.controller.js
+++ www/app/views/auth/signup/signup.controller.js
@@ -7,8 +7,43 @@
  */
 
 angular.module('app.auth.signup', [])
-        .controller('AuthSignupCtrl', ['$scope', '$state', function ($scope, $state) {
+        .controller('AuthSignupCtrl', ['$scope', '$state', '$log', 'AuthService', 
+        function ($scope, $log, $state, AuthService) {
         
         $scope.$state = $state;
+        $scope.form = {};
+
+        $scope.newUser = {
+            'nameFirst' : '',
+            'nameLast' : '',
+            'email' : '',
+            'password' : '',
+            'passwordB' : '',
+            'referer' : ''
+        };
+
+        $scope.signup = function() {
+            $scope.$broadcast('show-errors-check-validity');
+
+            if($scope.form.login.$valid) {
+                AuthService.signup($scope.newUser).then(function(results) {
+                    $log.debug(results);
+                }, function(error) {
+                    $log.debug(error);
+                });
+            } else {
+                $scope.form.signup.$setDirty();
+                $log.debug("Nope");
+            }
+        };
+
+        $scope.FacebookSignup = function() {
+            AuthService.facebookSignup().then(function (resp) {
+                $log.debug(resp);
+                $scope.newUser = resp;
+            }, function (err) {
+                $log.debug(err);
+            });
+        };
         
     }]);
