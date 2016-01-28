d455414bb63fa54a73482e93f05768ad55d4e777
Rachel L Carbone <rachellcarbone@gmail.com>
2016-01-28 14:00:58 -0500

    Finished debugging role routes controller.

--- api/v1/controllers/roles/roles.controller.php
+++ api/v1/controllers/roles/roles.controller.php
@@ -1,5 +1,7 @@
 <?php namespace API;
 require_once dirname(__FILE__) . '/roles.data.php';
+require_once dirname(dirname(__FILE__)) . '/groups/groups.data.php';
+require_once dirname(dirname(dirname(__FILE__))) . '/services/api.auth.php';
 
 use \Respect\Validation\Validator as v;
 
@@ -7,37 +9,97 @@ use \Respect\Validation\Validator as v;
 class RoleController {
 
     static function getRole($app, $roleId) {
-        $roles = RoleData::getRole($roleId);
-        if($roles) {
-            return $app->render(200, array('roles' => $roles));
+        if(!v::intVal()->validate($roleId)) {
+            return $app->render(400,  array('msg' => 'Could not select role. Check your parameters and try again.'));
+        }
+        $role = RoleData::getRole($roleId);
+        if($role) {
+            return $app->render(200, array('role' => $role));
         } else {
             return $app->render(400,  array('msg' => 'Could not select role.'));
         }
     }
     
     static function addRole($app) {
-        $roles = RoleData::insertRole();
-        if($roles) {
-            return $app->render(200, array('roles' => $roles));
+        // Validate parameters
+        if(!v::key('role', v::stringType())->validate($app->request->post()) || 
+           !v::key('desc', v::stringType())->validate($app->request->post())) {
+            // Validate input parameters
+            return $app->render(400, array('msg' => 'Add role failed. Check your parameters and try again.'));
+        }
+        
+        // Verify a unique slug
+        $slug = self::makeSlug($app->request->post('role'));
+        $existing = RoleData::selectRoleBySlug($slug);
+        if ($existing) {
+            return $app->render(400,  array('msg' => 'Could not add role. A role with that name already exists.', 'role' => $existing));
+        }
+        
+        // Add the verifed role   
+        $roleId = RoleData::insertRole(array (
+            ":role" => $app->request->post('role'),
+            ":slug" => self::makeSlug($app->request->post('role')),
+            ":desc" => $app->request->post('desc'),
+            ":created_user_id" => APIAuth::getUserId(),
+            ":last_updated_by" => APIAuth::getUserId()
+        ));
+        
+        // Return success
+        if($roleId) {
+            GroupData::addNewRoleToAdminGroup($roleId);
+            $role = RoleData::getRole($roleId);
+            return $app->render(200, array('role' => $role));
         } else {
-            return $app->render(400,  array('msg' => 'Could not select role.'));
+            return $app->render(400,  array('msg' => 'Could not add new role.'));
         }
     }
     
-    static function saveRole($app) {
-        $roles = RoleData::updateRole();
-        if($roles) {
-            return $app->render(200, array('roles' => $roles));
+    static function saveRole($app, $roleId) {
+        // Validate parameters
+        if(!v::intVal()->validate($roleId) || 
+           !v::key('role', v::stringType())->validate($app->request->post()) || 
+           !v::key('desc', v::stringType())->validate($app->request->post())) {
+            // Validate input parameters
+            return $app->render(400, array('msg' => 'Update role failed. Check your parameters and try again.'));
+        }
+        
+        // Verify a unique slug
+        $slug = self::makeSlug($app->request->post('role'));
+        $existing = RoleData::selectRoleBySlug($slug, $roleId);
+        if ($existing) {
+            return $app->render(400,  array('msg' => 'Could not edit role. A role with that name already exists.', 'role' => $existing));
+        }
+        
+        // Update the verifed role
+        $saved = RoleData::updateRole(array (
+            ':id' => $roleId,
+            ":role" => $app->request->post('role'),
+            ":slug" => $slug,
+            ":desc" => $app->request->post('desc'),
+            ":last_updated_by" => APIAuth::getUserId()
+        ));
+        
+        // Return success
+        if($saved) {
+            $role = RoleData::getRole($roleId);
+            return $app->render(200, array('role' => $role));
         } else {
-            return $app->render(400,  array('msg' => 'Could not select role.'));
+            return $app->render(400,  array('msg' => 'Could not update role.'));
         }
     }
     
     static function deleteRole($app, $roleId) {
-        if(RoleData::deleteRole($roleId)) {
+        if(!v::intVal()->validate($roleId)) {
+            return $app->render(400,  array('msg' => 'Could not delete role. Check your parameters and try again.'));
+        } else if(RoleData::deleteRole($roleId)) {
             return $app->render(200,  array('msg' => 'Role has been deleted.'));
         } else {
-            return $app->render(400,  array('msg' => 'Could not delete role. Check your parameters and try again.'));
+            return $app->render(400,  array('msg' => 'Could not delete role.'));
         }
     }
+
+    static function makeSlug($string) {
+        return preg_replace('/[^a-zA-Z0-9-_.]/', '', str_replace(' ', '-', trim($string)));
+    }
+    
 }
--- api/v1/controllers/roles/roles.data.php
+++ api/v1/controllers/roles/roles.data.php
@@ -3,23 +3,61 @@
 
 class RoleData {
   
+    // TODO: Add call that will verify that an admin role exists, 
+    // that the adming group has the role, 
+    // and the admin role is on every element. 
+    
     public static function getRole($id) {
-        $role = DBConn::selectOne("SELECT r.id, r.role, r.desc, r.created, r.last_updated AS lastUpdated, "
+        $role = DBConn::selectOne("SELECT r.id, r.role, r.desc, r.slug, r.created, r.last_updated AS lastUpdated, "
                 . "CONCAT(u1.name_first, ' ', u1.name_last) AS createdBy, "
                 . "CONCAT(u2.name_first, ' ', u2.name_last) AS updatedBy "
-                . "FROM as_auth_roles AS r "
-                . "JOIN as_users AS u1 ON u1.id = r.created_user_id "
-                . "JOIN as_users AS u2 ON u2.id = r.last_updated_by WHERE r.id = :id;", array(':id' => $id));
+                . "FROM " . DBConn::prefix() . "auth_roles AS r "
+                . "JOIN " . DBConn::prefix() . "users AS u1 ON u1.id = r.created_user_id "
+                . "JOIN " . DBConn::prefix() . "users AS u2 ON u2.id = r.last_updated_by WHERE r.id = :id LIMIT 1;", array(':id' => $id));
 
-        $qFields = DBConn::preparedQuery("SELECT e.id, e.identifier, e.desc "
-                . "FROM as_auth_fields AS e "
-                . "JOIN as_auth_lookup_role_field AS look ON e.id = look.auth_field_id "
+        $qFields = DBConn::preparedQuery("SELECT e.id, e.identifier, e.type, e.desc "
+                . "FROM " . DBConn::prefix() . "auth_fields AS e "
+                . "JOIN " . DBConn::prefix() . "auth_lookup_role_field AS look ON e.id = look.auth_field_id "
                 . "WHERE look.auth_role_id = :id ORDER BY e.identifier;");
         
         $qGroups = DBConn::preparedQuery("SELECT g.id, g.group, g.desc "
-                . "FROM as_auth_groups AS g "
-                . "JOIN as_auth_lookup_group_role AS look ON g.id = look.auth_group_id "
+                . "FROM " . DBConn::prefix() . "auth_groups AS g "
+                . "JOIN " . DBConn::prefix() . "auth_lookup_group_role AS look ON g.id = look.auth_group_id "
                 . "WHERE look.auth_role_id = :id ORDER BY g.group;");
+        
+        // TODDO: Add roles to this group query for a really robust view
+        
+        if($role) {
+            $qGroups->execute(array(':id' => $id));
+            $role->groups = $qGroups->fetchAll(\PDO::FETCH_OBJ);
+            
+            $qFields->execute(array(':id' => $id));
+            $role->elements = $qFields->fetchAll(\PDO::FETCH_OBJ);
+        }
+        return $role;
+    }
+  
+    public static function selectRoleBySlug($slug, $id = 0) {
+        $role = DBConn::selectOne("SELECT r.id, r.role, r.desc, r.slug, r.created, r.last_updated AS lastUpdated, "
+                . "CONCAT(u1.name_first, ' ', u1.name_last) AS createdBy, "
+                . "CONCAT(u2.name_first, ' ', u2.name_last) AS updatedBy "
+                . "FROM " . DBConn::prefix() . "auth_roles AS r "
+                . "JOIN " . DBConn::prefix() . "users AS u1 ON u1.id = r.created_user_id "
+                . "JOIN " . DBConn::prefix() . "users AS u2 ON u2.id = r.last_updated_by "
+                . "WHERE r.slug = :slug AND r.id != :id LIMIT 1;", 
+                array(':slug' => $slug, ':id' => $id));
+
+        $qFields = DBConn::preparedQuery("SELECT e.id, e.identifier, e.type, e.desc "
+                . "FROM " . DBConn::prefix() . "auth_fields AS e "
+                . "JOIN " . DBConn::prefix() . "auth_lookup_role_field AS look ON e.id = look.auth_field_id "
+                . "WHERE look.auth_role_id = :id ORDER BY e.identifier;");
+        
+        $qGroups = DBConn::preparedQuery("SELECT g.id, g.group, g.desc "
+                . "FROM " . DBConn::prefix() . "auth_groups AS g "
+                . "JOIN " . DBConn::prefix() . "auth_lookup_group_role AS look ON g.id = look.auth_group_id "
+                . "WHERE look.auth_role_id = :id ORDER BY g.group;");
+        
+        // TODDO: Add roles to this group query for a really robust view
         
         if($role) {
             $qGroups->execute(array(':id' => $id));
@@ -32,20 +70,19 @@ class RoleData {
     }
   
     public static function insertRole($validRole) {
-        return DBConn::insert("INSERT INTO as_auth_roles(role, slug, desc, created_user_id, last_updated_by) "
-                . "VALUES (:role, :slug, :desc, :created_user_id, :last_updated_by)", $validRole);
+        return DBConn::insert("INSERT INTO " . DBConn::prefix() . "auth_roles(`role`, `slug`, `desc`, `created_user_id`, `last_updated_by`) "
+                . "VALUES (:role, :slug, :desc, :created_user_id, :last_updated_by);", $validRole);
     }
     
     public static function updateRole($validRole) {
-        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_roles SET role=:role, slug=:slug, "
-                . "desc=:desc, last_updated_by=:last_updated_by;", $validRole);
+        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_roles SET `role`=:role, `slug`=:slug, "
+                . "`desc`=:desc, `last_updated_by`=:last_updated_by WHERE `id`=:id;", $validRole);
     }
     
     public static function deleteRole($id) {
-        $fields = DBConn::delete("DELETE FROM " . DBConn::prefix() . "auth_lookup_role_field WHERE auth_role_id = :id;", array('id' => $id));
-        $groups = DBConn::delete("DELETE FROM " . DBConn::prefix() . "auth_lookup_group_role WHERE auth_role_id = :id;", array('id' => $id));
+        $fields = DBConn::delete("DELETE FROM " . DBConn::prefix() . "auth_lookup_role_field WHERE `auth_role_id`= :id;", array(':id' => $id));
+        $groups = DBConn::delete("DELETE FROM " . DBConn::prefix() . "auth_lookup_group_role WHERE `auth_role_id` = :id;", array(':id' => $id));
         
-        return (!$fields || !$groups)  ? false :
-            DBConn::delete("DELETE FROM " . DBConn::prefix() . "auth_roles WHERE id = :id LIMIT 1;", array('id' => $id));
+        return DBConn::delete("DELETE FROM " . DBConn::prefix() . "auth_roles WHERE `id` = :id LIMIT 1;", array(':id' => $id));
     }
 }
