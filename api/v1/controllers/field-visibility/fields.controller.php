aa3b433d6fcbc88eb71a534d2423e0a249aedcd0
Rachel L Carbone <rachellcarbone@gmail.com>
2016-01-28 16:00:39 -0500

    Completed field routes.

--- api/v1/controllers/field-visibility/fields.controller.php
+++ api/v1/controllers/field-visibility/fields.controller.php
@@ -1,5 +1,7 @@
 <?php namespace API;
 require_once dirname(__FILE__) . '/fields.data.php';
+require_once dirname(dirname(__FILE__)) . '/roles/roles.data.php';
+require_once dirname(dirname(dirname(__FILE__))) . '/services/api.auth.php';
 
 use \Respect\Validation\Validator as v;
 
@@ -8,7 +10,7 @@ class FieldController {
 
     static function getField($app, $fieldId) {
         if(!v::intVal()->validate($fieldId)) {
-            return $app->render(400,  array('msg' => 'Could not find field.'));
+            return $app->render(400,  array('msg' => 'Could not select field. Check your parameters and try again.'));
         }
         $field = FieldData::getField($fieldId);
         if($field) {
@@ -19,30 +21,103 @@ class FieldController {
     }
     
     static function addField($app) {
-        $field = FieldData::insertField();
-        if($field) {
+        if(!v::key('identifier', v::stringType())->validate($app->request->post()) || 
+           !v::key('type', v::stringType())->validate($app->request->post()) || 
+           !v::key('desc', v::stringType())->validate($app->request->post())) {
+            // Validate input parameters
+            return $app->render(400, array('msg' => 'Add failed. Check your parameters and try again.'));
+        } else if(strtolower($app->request->post('type')) != 'state' && strtolower($app->request->post('type')) != 'element') {
+            return $app->render(400, array('msg' => 'Add failed. Invalid field type. Only "state" and "element" are allowed.'));
+        }
+        
+        // Verify a unique slug
+        $identifier = self::makeSlug($app->request->post('identifier'));
+        $existing = FieldData::getByIdentifier($identifier);
+        if ($existing) {
+            return $app->render(400,  array('msg' => 'Could not add field. A field with that name already exists.', 'field' => $existing));
+        }
+        
+        // Add Cariable
+        $data = array (
+            ':identifier' => $identifier,
+            ":type" => strtolower($app->request->post('type')),
+            ":desc" => $app->request->post('desc'),
+            ":created_user_id" => APIAuth::getUserId(),
+            ":last_updated_by" => APIAuth::getUserId()
+        );
+        
+        $fieldId = FieldData::insertField($data);
+        if($fieldId) {
+            RoleData::addAdminRoleToNewField($fieldId);
+            $field = FieldData::getField($fieldId);
             return $app->render(200, array('field' => $field));
         } else {
-            return $app->render(400,  array('msg' => 'Could not select field.'));
+            return $app->render(400,  array('msg' => 'Could not add field.'));
         }
     }
     
-    static function saveField($app) {
-        $field = FieldData::updateField();
-        if($field) {
+    static function saveField($app, $fieldId) {
+        if(!v::intVal()->validate($fieldId) || 
+           !v::key('identifier', v::stringType())->validate($app->request->post()) || 
+           !v::key('type', v::stringType())->validate($app->request->post()) || 
+           !v::key('desc', v::stringType())->validate($app->request->post())) {
+            // Validate input parameters
+            return $app->render(400, array('msg' => 'Update failed. Check your parameters and try again.'));
+        } else if(strtolower($app->request->post('type')) != 'state' && strtolower($app->request->post('type')) != 'element') {
+            return $app->render(400, array('msg' => 'Update failed. Invalid field type. Only "state" and "element" are allowed.'));
+        }
+        
+        // Verify a unique slug
+        $identifier = self::makeSlug($app->request->post('identifier'));
+        $existing = FieldData::getByIdentifier($identifier, $fieldId);
+        if ($existing) {
+            return $app->render(400,  array('msg' => 'Could not update field. A field with that name already exists.', 'field' => $existing));
+        }
+        
+        // Add Cariable
+        $data = array (
+            ':id' => $fieldId,
+            ':identifier' => $identifier,
+            ":type" => strtolower($app->request->post('type')),
+            ":desc" => $app->request->post('desc'),
+            ":last_updated_by" => APIAuth::getUserId()
+        );
+        
+        $saved = FieldData::updateField($data);
+        if($saved) {
+            RoleData::addAdminRoleToNewField($fieldId);
+            $field = FieldData::getField($fieldId);
             return $app->render(200, array('field' => $field));
         } else {
-            return $app->render(400,  array('msg' => 'Could not select field.'));
+            return $app->render(400,  array('msg' => 'Could not update field.'));
+        }
+    }
+    
+    static function initializeField($app, $fieldId) {
+        if(!v::intVal()->validate($fieldId)) {
+           return $app->render(400, array('msg' => 'Field initialization failed. Check your parameters and try again.'));
+        }
+        
+        $saved = FieldData::updateFieldInitialize(array(':id' => $fieldId, ':initialized' => date('Y-m-d H:i:s')));
+        if($saved) {
+            $field = FieldData::getField($fieldId);
+            return $app->render(200, array('field' => $field));
+        } else {
+            return $app->render(400,  array('msg' => 'Could not initialize field.'));
         }
     }
     
     static function deleteField($app, $fieldId) {
         if(!v::intVal()->validate($fieldId)) {
-            return $app->render(400,  array('msg' => 'Could not find field.'));
+            return $app->render(400,  array('msg' => 'Could not delete field. Check your parameters and try again.'));
         } else if(FieldData::deleteField($fieldId)) {
             return $app->render(200,  array('msg' => 'Field has been deleted.'));
         } else {
-            return $app->render(400,  array('msg' => 'Could not delete field. Check your parameters and try again.'));
+            return $app->render(400,  array('msg' => 'Could not delete field.'));
         }
+    }
+    
+    static function makeSlug($string) {
+        return preg_replace('/[^a-zA-Z0-9-_.]/', '', str_replace(' ', '.', strtolower(trim($string))));
     }
 }
--- api/v1/controllers/field-visibility/fields.data.php
+++ api/v1/controllers/field-visibility/fields.data.php
@@ -8,7 +8,30 @@ class FieldData {
                 . "CONCAT(u1.name_first, ' ', u1.name_last) AS createdBy, CONCAT(u2.name_first, ' ', u2.name_last) AS updatedBy "
                 . "FROM " . DBConn::prefix() . "auth_fields AS f "
                 . "JOIN " . DBConn::prefix() . "users AS u1 ON u1.id = f.created_user_id "
-                . "JOIN " . DBConn::prefix() . "users AS u2 ON u2.id = f.last_updated_by WHERE f.id = :id;", array(':id' => $id));
+                . "JOIN " . DBConn::prefix() . "users AS u2 ON u2.id = f.last_updated_by "
+                . "WHERE f.id = :id LIMIT 1;", array(':id' => $id));
+
+        $qRoles = DBConn::preparedQuery("SELECT r.id, r.role, r.desc "
+                . "FROM " . DBConn::prefix() . "auth_roles AS r "
+                . "JOIN " . DBConn::prefix() . "auth_lookup_role_field AS look ON r.id = look.auth_role_id "
+                . "WHERE look.auth_field_id = :id ORDER BY r.role;");
+        
+        if($field) {          
+            $qRoles->execute(array(':id' => $field->id));
+            $field->roles = $qRoles->fetchAll(\PDO::FETCH_OBJ);
+        }
+        
+        return $field;
+    }
+    
+    public static function getByIdentifier($identifier, $fieldId = 0) {
+        $field = DBConn::selectOne("SELECT f.id, f.identifier, f.type, f.desc, f.initialized, f.created, f.last_updated AS lastUpdated, "
+                . "CONCAT(u1.name_first, ' ', u1.name_last) AS createdBy, CONCAT(u2.name_first, ' ', u2.name_last) AS updatedBy "
+                . "FROM " . DBConn::prefix() . "auth_fields AS f "
+                . "JOIN " . DBConn::prefix() . "users AS u1 ON u1.id = f.created_user_id "
+                . "JOIN " . DBConn::prefix() . "users AS u2 ON u2.id = f.last_updated_by "
+                . "WHERE f.identifier = :identifier AND f.id ! = :id "
+                . "LIMIT 1;", array(':identifier' => $identifier, ':id' => $fieldId));
 
         $qRoles = DBConn::preparedQuery("SELECT r.id, r.role, r.desc "
                 . "FROM " . DBConn::prefix() . "auth_roles AS r "
@@ -24,13 +47,17 @@ class FieldData {
     }
   
     public static function insertField($validField) {
-        return DBConn::insert("INSERT INTO as_auth_fields(identifier, type, desc, created_user_id, last_updated_by) "
+        return DBConn::insert("INSERT INTO " . DBConn::prefix() . "auth_fields(`identifier`, `type`, `desc`, `created_user_id`, `last_updated_by`) "
                 . "VALUES (:identifier, :type, :desc, :created_user_id, :last_updated_by)", $validField);
     }
     
+    public static function updateFieldInitialize($validField) {
+        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_fields SET `initialized`=:initialized WHERE `id`=:id;", $validField);
+    }
+    
     public static function updateField($validField) {
-        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_fields SET identifier=:identifier, type=:type, "
-                . "desc=:desc, initialized=:initialized, last_updated_by=:last_updated_by;", $validField);
+        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_fields SET `identifier`=:identifier, `type`=:type, "
+                . "`desc`=:desc, `last_updated_by`=:last_updated_by, initialized=NULL WHERE `id`=:id;", $validField);
     }
     
     public static function deleteField($id) {
