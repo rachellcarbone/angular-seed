e08b63df64730ee885ae197af77600fdbf36ae09
Rachel L Carbone <rachellcarbone@gmail.com>
2016-01-28 14:02:03 -0500

    Added route param comments for future auto docs.

--- api/v1/controllers/field-visibility/fields.routes.php
+++ api/v1/controllers/field-visibility/fields.routes.php
@@ -9,18 +9,30 @@ class FieldRoutes {
         
         $app->group('/field', $authenticateForRole('admin'), function () use ($app) {
             
+            /*
+             * id
+             */
             $app->map("/get/:fieldId/", function ($fieldId) use ($app) {
                 FieldController::getField($app, $fieldId);
             })->via('GET', 'POST');
 
+            /*
+             * identifier, type, desc
+             */
             $app->post("/insert/", function () use ($app) {
                 FieldController::addField($app);
             });
 
+            /*
+             * id, identifier, type, desc
+             */
             $app->post("/update/:fieldId/", function ($fieldId) use ($app) {
                 FieldController::saveField($app, $fieldId);
             });
 
+            /*
+             * id
+             */
             $app->map("/delete/:fieldId/", function ($fieldId) use ($app) {
                 FieldController::deleteField($app, $fieldId);
             })->via('DELETE', 'POST');
--- api/v1/controllers/groups/groups.routes.php
+++ api/v1/controllers/groups/groups.routes.php
@@ -9,18 +9,30 @@ class GroupRoutes {
         
         $app->group('/group', $authenticateForRole('admin'), function () use ($app) {
             
+            /*
+             * id
+             */
             $app->map("/get/:groupId/", function ($groupId) use ($app) {
                 GroupController::getGroup($app, $groupId);
             })->via('GET', 'POST');
 
+            /*
+             * group, desc
+             */
             $app->post("/insert/", function () use ($app) {
                 GroupController::addGroup($app);
             });
 
+            /*
+             * id, group, desc
+             */
             $app->post("/update/:groupId/", function ($groupId) use ($app) {
                 GroupController::saveGroup($app, $groupId);
             });
 
+            /*
+             * id
+             */
             $app->map("/delete/:groupId/", function ($groupId) use ($app) {
                 GroupController::deleteGroup($app, $groupId);
             })->via('DELETE', 'POST');
