7c90865b0a7e47695b869dd39c64861e5e31adc6
Rachel L Carbone <rachellcarbone@gmail.com>
2016-01-28 14:22:39 -0500

    Finished debugging group routes.

--- api/v1/controllers/groups/groups.controller.php
+++ api/v1/controllers/groups/groups.controller.php
@@ -1,5 +1,6 @@
 <?php namespace API;
 require_once dirname(__FILE__) . '/groups.data.php';
+require_once dirname(dirname(dirname(__FILE__))) . '/services/api.auth.php';
 
 use \Respect\Validation\Validator as v;
 
@@ -7,37 +8,96 @@ use \Respect\Validation\Validator as v;
 class GroupController {
 
     static function getGroup($app, $groupId) {
-        $groups = GroupData::getGroup($groupId);
-        if($groups) {
-            return $app->render(200, array('groups' => $groups));
+        if(!v::intVal()->validate($groupId)) {
+            return $app->render(400,  array('msg' => 'Could not select group. Check your parameters and try again.'));
+        }
+        $group = GroupData::getGroup($groupId);
+        if($group) {
+            return $app->render(200, array('group' => $group));
         } else {
             return $app->render(400,  array('msg' => 'Could not select group.'));
         }
     }
     
     static function addGroup($app) {
-        $groups = GroupData::insertGroup();
-        if($groups) {
-            return $app->render(200, array('groups' => $groups));
+        // Validate parameters
+        if(!v::key('group', v::stringType())->validate($app->request->post()) || 
+           !v::key('desc', v::stringType())->validate($app->request->post())) {
+            // Validate input parameters
+            return $app->render(400, array('msg' => 'Add failed. Check your parameters and try again.'));
+        }
+        
+        // Verify a unique slug
+        $slug = self::makeSlug($app->request->post('group'));
+        $existing = GroupData::selectGroupBySlug($slug);
+        if ($existing) {
+            return $app->render(400,  array('msg' => 'Could not add group. A group with that name already exists.', 'group' => $existing));
+        }
+        
+        // Add the verifed group   
+        $groupId = GroupData::insertGroup(array(
+            ":group" => $app->request->post('group'),
+            ":slug" => $slug,
+            ":desc" => $app->request->post('desc'),
+            ":created_user_id" => APIAuth::getUserId(),
+            ":last_updated_by" => APIAuth::getUserId()
+        ));
+        
+        // Return success
+        if($groupId) {
+            GroupData::addPublicRoleToNewGroup($groupId);
+            $group = GroupData::getGroup($groupId);
+            return $app->render(200, array('group' => $group));
         } else {
-            return $app->render(400,  array('msg' => 'Could not select group.'));
+            return $app->render(400,  array('msg' => 'Could not add group.'));
         }
     }
     
-    static function saveGroup($app) {
-        $groups = GroupData::updateGroup();
-        if($groups) {
-            return $app->render(200, array('groups' => $groups));
+    static function saveGroup($app, $groupId) {
+        // Validate parameters
+        if(!v::intVal()->validate($groupId) || 
+           !v::key('group', v::stringType())->validate($app->request->post()) || 
+           !v::key('desc', v::stringType())->validate($app->request->post())) {
+            // Validate input parameters
+            return $app->render(400, array('msg' => 'Update failed. Check your parameters and try again.'));
+        }
+        
+        // Verify a unique slug
+        $slug = self::makeSlug($app->request->post('group'));
+        $existing = GroupData::selectGroupBySlug($slug, $groupId);
+        if ($existing) {
+            return $app->render(400,  array('msg' => 'Could not update group. A group with that name already exists.', 'group' => $existing));
+        }
+        
+        // Update the verifed group   
+        $saved = GroupData::updateGroup(array(
+            ':id' => $groupId,
+            ":group" => $app->request->post('group'),
+            ":slug" => $slug,
+            ":desc" => $app->request->post('desc'),
+            ":last_updated_by" => APIAuth::getUserId()
+        ));
+        
+        // Return success
+        if($saved) {
+            $group = GroupData::getGroup($groupId);
+            return $app->render(200, array('group' => $group));
         } else {
-            return $app->render(400,  array('msg' => 'Could not select group.'));
+            return $app->render(400,  array('msg' => 'Could not update group.'));
         }
     }
     
     static function deleteGroup($app, $groupId) {
-        if(GroupData::deleteGroup($groupId)) {
+        if(!v::intVal()->validate($groupId)) {
+            return $app->render(400,  array('msg' => 'Could not delete group. Check your parameters and try again.'));
+        } else if(GroupData::deleteGroup($groupId)) {
             return $app->render(200,  array('msg' => 'Group has been deleted.'));
         } else {
-            return $app->render(400,  array('msg' => 'Could not delete group. Check your parameters and try again.'));
+            return $app->render(400,  array('msg' => 'Could not delete group.'));
         }
+    }
+    
+    static function makeSlug($string) {
+        return preg_replace('/[^a-zA-Z0-9-_.]/', '', str_replace(' ', '-', strtolower(trim($string))));
     }
 }
--- api/v1/controllers/groups/groups.data.php
+++ api/v1/controllers/groups/groups.data.php
@@ -4,42 +4,60 @@
 class GroupData {
     
     static function getGroup($id) {        
-        $group = DBConn::selectOne("SELECT g.id, g.group, g.desc, g.created, g.last_updated AS lastUpdated, "
+        $group = DBConn::selectOne("SELECT g.id, g.group, g.slug, g.desc, g.created, g.last_updated AS lastUpdated, "
                         . "CONCAT(u1.name_first, ' ', u1.name_last) AS createdBy, "
                         . "CONCAT(u2.name_first, ' ', u2.name_last) AS updatedBy "
-                        . "FROM as_auth_groups AS g "
-                        . "JOIN as_users AS u1 ON u1.id = g.created_user_id "
-                        . "JOIN as_users AS u2 ON u2.id = g.last_updated_by WHERE g.id = :id;", array(':id' => $id));
+                        . "FROM " . DBConn::prefix() . "auth_groups AS g "
+                        . "JOIN " . DBConn::prefix() . "users AS u1 ON u1.id = g.created_user_id "
+                        . "JOIN " . DBConn::prefix() . "users AS u2 ON u2.id = g.last_updated_by WHERE g.id = :id;", array(':id' => $id));
         if($group) {
             $group->roles = DBConn::selectAll("SELECT r.id, r.role, r.desc "
-                        . "FROM as_auth_roles AS r "
-                        . "JOIN as_auth_lookup_group_role AS look ON r.id = look.auth_role_id "
+                        . "FROM " . DBConn::prefix() . "auth_roles AS r "
+                        . "JOIN " . DBConn::prefix() . "auth_lookup_group_role AS look ON r.id = look.auth_role_id "
+                        . "WHERE look.auth_group_id = :id ORDER BY r.role;", array(':id' => $id));
+        }
+        return $group;
+    }
+    
+    // TODO: Double check that limit one is on all selectOne calls to speed up queries
+    static function selectGroupBySlug($slug, $id = 0) {        
+        $group = DBConn::selectOne("SELECT g.id, g.group, g.slug, g.desc, g.created, g.last_updated AS lastUpdated, "
+                        . "CONCAT(u1.name_first, ' ', u1.name_last) AS createdBy, "
+                        . "CONCAT(u2.name_first, ' ', u2.name_last) AS updatedBy "
+                        . "FROM " . DBConn::prefix() . "auth_groups AS g "
+                        . "JOIN " . DBConn::prefix() . "users AS u1 ON u1.id = g.created_user_id "
+                        . "JOIN " . DBConn::prefix() . "users AS u2 ON u2.id = g.last_updated_by "
+                        . "WHERE g.slug = :slug AND g.id != :id LIMIT 1;", 
+                        array(':slug' => $slug, ':id' => $id));
+        if($group) {
+            $group->roles = DBConn::selectAll("SELECT r.id, r.role, r.desc "
+                        . "FROM " . DBConn::prefix() . "auth_roles AS r "
+                        . "JOIN " . DBConn::prefix() . "auth_lookup_group_role AS look ON r.id = look.auth_role_id "
                         . "WHERE look.auth_group_id = :id ORDER BY r.role;", array(':id' => $id));
         }
         return $group;
     }
     
     static function insertGroup($validGroup) {
-        return DBConn::insert("INSERT INTO as_auth_groups(group, desc, created_user_id, last_updated_by) 
-            VALUES (:group, :desc, :created_user_id, :last_updated_by);", $validGroup);
+        return DBConn::insert("INSERT INTO " . DBConn::prefix() . "auth_groups(`group`, `desc`, `slug`, `created_user_id`, `last_updated_by`) "
+                . "VALUES (:group, :desc, :slug, :created_user_id, :last_updated_by);", $validGroup);
     }
     
     static function updateGroup($validGroup) {
-        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_groups SET group=:group, desc=:desc, "
-                . "last_updated_by=:last_updated_by;", $validGroup);
+        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_groups SET `group`=:group, `slug`=:slug, `desc`=:desc, "
+                . "`last_updated_by`=:last_updated_by WHERE `id`=:id;", $validGroup);
     }
     
     static function deleteGroup($id) {
         $users = DBConn::delete("DELETE FROM " . DBConn::prefix() . "auth_lookup_user_group WHERE auth_group_id = :id;", array('id' => $id));
         $roles = DBConn::delete("DELETE FROM " . DBConn::prefix() . "auth_lookup_group_role WHERE auth_group_id = :id;", array('id' => $id));
-        
-        return (!$users || !$roles)  ? false :
-            DBConn::delete("DELETE FROM " . DBConn::prefix() . "auth_groups WHERE id = :id LIMIT 1;", array('id' => $id));
+        return DBConn::delete("DELETE FROM " . DBConn::prefix() . "auth_groups WHERE id = :id LIMIT 1;", array('id' => $id));
     }
     
     static function addDefaultGroupToUser($userId) {
+        // TODO: Hook up default group to config variable
         $groupId = DBConn::selectOne("SELECT g.id FROM " . DBConn::prefix() . "auth_groups AS g "
-                . "WHERE g.group LIKE :group;", array(':group' => 'member'));
+                . "WHERE g.slug = :slug;", array(':slug' => 'member'));
         
         if(!$groupId) {
             $groupId = DBConn::selectOne("SELECT g.id FROM " . DBConn::prefix() . "auth_groups AS g "
@@ -52,8 +70,44 @@ class GroupData {
                 ':auth_group_id' => $groupId->id, 
                 ':created_user_id' => $userId
             );
-            return DBConn::insert("INSERT INTO as_auth_lookup_user_group(user_id, auth_group_id, created_user_id) "
+            return DBConn::insert("INSERT INTO " . DBConn::prefix() . "auth_lookup_user_group(user_id, auth_group_id, created_user_id) "
                     . "VALUES (:user_id, :auth_group_id, :created_user_id);", $validGroup);
+        }
+        
+        return false;
+    }
+    
+    static function addNewRoleToAdminGroup($roleId) {
+        // TODO: Hook up super-admin group to config variable
+        $groupId = DBConn::selectOne("SELECT g.id FROM " . DBConn::prefix() . "auth_groups AS g "
+                . "WHERE g.slug = :slug LIMIT 1;", array(':slug' => 'super-admin'));
+        
+        if($groupId) {
+            $validGroup = array(
+                ':auth_group_id' => $groupId->id, 
+                ':auth_role_id' => $roleId,
+                ':created_user_id' => APIAuth::getUserId()
+            );
+            return DBConn::insert("INSERT INTO " . DBConn::prefix() . "auth_lookup_group_role(auth_group_id, auth_role_id, created_user_id) "
+                    . "VALUES (:auth_group_id, :auth_role_id, :created_user_id);", $validGroup);
+        }
+        
+        return false;
+    }
+    
+    static function addPublicRoleToNewGroup($groupId) {
+        // TODO: Hook up super-admin group to config variable
+        $roleId = DBConn::selectOne("SELECT r.id FROM " . DBConn::prefix() . "auth_roles AS r "
+                . "WHERE r.slug = :slug LIMIT 1;", array(':slug' => 'public'));
+        
+        if($roleId) {
+            $validGroup = array(
+                ':auth_group_id' => $groupId, 
+                ':auth_role_id' => $roleId->id,
+                ':created_user_id' => APIAuth::getUserId()
+            );
+            return DBConn::insert("INSERT INTO " . DBConn::prefix() . "auth_lookup_group_role(auth_group_id, auth_role_id, created_user_id) "
+                    . "VALUES (:auth_group_id, :auth_role_id, :created_user_id);", $validGroup);
         }
         
         return false;
