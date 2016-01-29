8c742087c8fb7636f39341184f4dcbc21a2f06b0
Rachel L Carbone <rachellcarbone@gmail.com>
2016-01-29 13:55:06 -0500

    I want the composer autoload to be the first thing that is required in the api.

--- api/v1/controllers/api.v1.php
+++ api/v1/controllers/api.v1.php
@@ -1,7 +1,6 @@
 <?php
 namespace API;
 
-require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';   // Composer components
 require_once dirname(dirname(__FILE__)) . '/config/config.php';     // API Coifg File (Add your settings!)
 require_once dirname(dirname(__FILE__)) . '/services/logging.php'; // Router Module
 require_once dirname(dirname(__FILE__)) . '/services/api.auth.php'; // Auth Service
--- api/v1/index.php
+++ api/v1/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once dirname(__FILE__) . '/vendor/autoload.php';   // Composer components
 require_once dirname(__FILE__) . '/scripts/phpErrorHandling.php';  // Logging Service
 require_once dirname(__FILE__) . '/controllers/api.v1.php';
 
