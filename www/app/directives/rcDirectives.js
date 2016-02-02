7c528ddc6831cad4868ebc17c658dbe174a2d0c2
Rachel L Carbone <rachellcarbone@gmail.com>
2016-02-02 13:23:15 -0500

    Added string-to-number directive to be used on input[number]

--- www/app/directives/rcDirectives.js
+++ www/app/directives/rcDirectives.js
@@ -16,4 +16,17 @@ angular.module('rcDirectives', [
     var scripts = document.getElementsByTagName("script");
     var scriptPath = scripts[scripts.length - 1].src;
     return scriptPath.substring(0, scriptPath.lastIndexOf('/') + 1);
-})());
+})())
+.directive('stringToNumber', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModel) {
+            ngModel.$parsers.push(function (value) {
+                return '' + value;
+            });
+            ngModel.$formatters.push(function (value) {
+                return parseFloat(value, 10);
+            });
+        }
+    };
+});
