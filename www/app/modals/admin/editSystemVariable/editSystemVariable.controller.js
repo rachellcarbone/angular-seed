f74ac7f1b37c86e4dfa656446cc5f3a49e37b912
Rachel L Carbone <rachellcarbone@gmail.com>
2016-02-06 01:30:35 -0500

    Added more form validation to system variables page.

--- www/app/modals/admin/editSystemVariable/editSystemVariable.controller.js
+++ www/app/modals/admin/editSystemVariable/editSystemVariable.controller.js
@@ -31,9 +31,28 @@ angular.module('app.modal.editSystemVariable', [])
             });
     };
     
+    $scope.checkboxLockedWarning = function($event, IsAccepted) {
+        if ($event !== undefined) {
+
+            var checkbox = $event.target;
+
+            if (checkbox.checked) {
+
+                AlertConfirmService.confirm('Are you sure you want to lock this variable? I can only be changed by admin with the correct permissions.', 'Restricting Access')
+                    .result.then(function () {
+                    }, function (declined) {
+                        checkbox.checked = false;
+                    });
+
+            }
+
+        }
+
+    };
+    
     /* Click event for the Save button */
     $scope.buttonSave = function() {
-        AlertConfirmService.confirm('Are you sure you want to change this variable? It may effect system settings.')
+        AlertConfirmService.confirm('Are you sure you want to change this variable? It may effect system settings.', 'System Wide Setting')
             .result.then(function () {
                 ApiRoutesSystemVariables.saveSystemVariable($scope.variable).then(
                     function (result) {
@@ -48,7 +67,7 @@ angular.module('app.modal.editSystemVariable', [])
     
     /* Click event for the Delete button */
     $scope.buttonDelete = function() {
-        AlertConfirmService.confirm('Are you sure you want to delete this variable? It may effect system settings.')
+        AlertConfirmService.confirm('Are you sure you want to delete this variable? It may effect system settings.', 'Delete Warning')
             .result.then(function () {
                 ApiRoutesSystemVariables.deleteSystemVariable($scope.variable.id).then(
                     function (result) {
