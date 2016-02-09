23da24033417ead0cf949ab85e3835ab43d8d7fe
Rachel L Carbone <rachellcarbone@gmail.com>
2016-02-08 19:09:12 -0500

    Cleanup

--- www/app/modals/admin/editVisibilityField/editVisibilityField.controller.js
+++ www/app/modals/admin/editVisibilityField/editVisibilityField.controller.js
@@ -32,22 +32,6 @@ angular.module('app.modal.editVisibilityField', [])
             });
     };
     
-    $scope.checkboxLockedWarning = function($event, IsAccepted) {
-        if ($event !== undefined) {
-
-            var checkbox = $event.target;
-
-            if (checkbox.checked) {
-
-                AlertConfirmService.confirm('Are you sure you want to lock this variable? I can only be changed by admin with the correct permissions.', 'Restricting Access')
-                    .result.then(function () {
-                    }, function (declined) {
-                        checkbox.checked = false;
-                    });
-            }
-        }
-    };
-    
     /* Click event for the Save button */
     $scope.buttonSave = function() {
         AlertConfirmService.confirm('Are you sure you want to change this variable? It may effect system settings.', 'System Wide Setting')
