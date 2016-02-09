da4b2e5940f1447716246298d50256fa1ed405d4
Rachel L Carbone <rachellcarbone@gmail.com>
2016-02-08 22:18:06 -0500

    Added modes to modals to allow management of referenced fields.

--- www/app/modals/admin/editRole/editRole.controller.js
+++ www/app/modals/admin/editRole/editRole.controller.js
@@ -12,9 +12,54 @@ angular.module('app.modal.editRole', [])
     /* Holds the add / edit form on the modal */
     $scope.form = {};
     
-    /* Is the modal in edit mode? Shows / Hides form */
-    $scope.editMode = (!angular.isDefined(editing.id));
-    $scope.newMode = (!angular.isDefined(editing.id));
+    /* Modal Mode */    
+    $scope.setMode = function(type) {
+        $scope.viewMode = false;
+        $scope.newMode = false;
+        $scope.editMode = false;
+        $scope.manageFieldsMode = false;
+        $scope.manageGroupsMode = false;
+        
+        switch(type) {
+            case 'new':
+                $scope.newMode = true;
+                break;
+            case 'edit':
+                $scope.editMode = true;
+                break;
+            case 'fields':
+                $scope.manageFieldsMode = true;
+                break;
+            case 'groups':
+                $scope.manageGroupsMode = true;
+                break;
+            case 'view':
+            default:
+                $scope.viewMode = true;
+                break;
+        }
+    };
+    
+    if(angular.isDefined(editing.id)) {
+        $scope.setMode('view');
+    } else {
+        $scope.setMode('new');
+    }
+    
+    $scope.getMode = function() {
+        if($scope.newMode) {
+            return 'new';
+        } else if($scope.editMode) {
+            return 'edit';
+        } else if($scope.manageFieldsMode) {
+            return 'fields';
+        } else if($scope.manageGroupsMode) {
+            return 'groups';
+        } else {
+            return 'view';
+        }
+    };
+    
     
     /* Save for resetting purposes */
     $scope.saved = (angular.isDefined(editing.id)) ? angular.copy(editing) : {
@@ -64,19 +109,33 @@ angular.module('app.modal.editRole', [])
                 $scope.alertProxy.info('No changes were saved.');
             });
     };
+    
+    /* Click event for the Manage Roles button */
+    $scope.buttonManageRoles = function() {
+        $scope.setMode('roles');
+    };
         
     /* Click event for the Cancel button */
-    $scope.buttonCancel = function() {    
-        if($scope.newMode || !$scope.editMode) {
-            $uibModalInstance.dismiss(false);
-        } else {
-            $scope.editMode = false;
-        }
+    $scope.buttonCancel = function() {
+        var mode = $scope.getMode();
+        
+        switch(mode) {
+            case 'edit':
+            case 'fields':
+            case 'groups':
+                $scope.setMode('view');
+                break;
+            case 'new':
+            case 'view':
+            default:
+                $uibModalInstance.dismiss(false);
+                break;
+        };
     };
     
     /* Click event for the Edit button*/
     $scope.buttonEdit = function() {
-        $scope.editMode = true;
+        $scope.setMode('edit');
     };
     
 }]);
--- www/app/modals/admin/editVisibilityField/editVisibilityField.controller.js
+++ www/app/modals/admin/editVisibilityField/editVisibilityField.controller.js
@@ -12,9 +12,48 @@ angular.module('app.modal.editVisibilityField', [])
     /* Holds the add / edit form on the modal */
     $scope.form = {};
     
-    /* Is the modal in edit mode? Shows / Hides form */
-    $scope.editMode = (!angular.isDefined(editing.id));
-    $scope.newMode = (!angular.isDefined(editing.id));
+    /* Modal Mode */    
+    $scope.setMode = function(type) {
+        $scope.viewMode = false;
+        $scope.newMode = false;
+        $scope.editMode = false;
+        $scope.manageRolesMode = false;
+        
+        switch(type) {
+            case 'new':
+                $scope.newMode = true;
+                break;
+            case 'edit':
+                $scope.editMode = true;
+                break;
+            case 'roles':
+                $scope.manageRolesMode = true;
+                break;
+            case 'view':
+            default:
+                $scope.viewMode = true;
+                break;
+        }
+    };
+    
+    if(angular.isDefined(editing.id)) {
+        $scope.setMode('view');
+    } else {
+        $scope.setMode('new');
+    }
+    
+    $scope.getMode = function() {
+        if($scope.newMode) {
+            return 'new';
+        } else if($scope.editMode) {
+            return 'edit';
+        } else if($scope.manageRolesMode) {
+            return 'roles';
+        } else {
+            return 'view';
+        }
+    };
+    
     
     /* Save for resetting purposes */
     $scope.saved = (angular.isDefined(editing.id)) ? angular.copy(editing) : {
@@ -65,19 +104,32 @@ angular.module('app.modal.editVisibilityField', [])
                 $scope.alertProxy.info('No changes were saved.');
             });
     };
+    
+    /* Click event for the Manage Roles button */
+    $scope.buttonManageRoles = function() {
+        $scope.setMode('roles');
+    };
         
     /* Click event for the Cancel button */
-    $scope.buttonCancel = function() {    
-        if($scope.newMode || !$scope.editMode) {
-            $uibModalInstance.dismiss(false);
-        } else {
-            $scope.editMode = false;
-        }
+    $scope.buttonCancel = function() {
+        var mode = $scope.getMode();
+        
+        switch(mode) {
+            case 'edit':
+            case 'roles':
+                $scope.setMode('view');
+                break;
+            case 'new':
+            case 'view':
+            default:
+                $uibModalInstance.dismiss(false);
+                break;
+        };
     };
     
     /* Click event for the Edit button*/
     $scope.buttonEdit = function() {
-        $scope.editMode = true;
+        $scope.setMode('edit');
     };
     
 }]);
