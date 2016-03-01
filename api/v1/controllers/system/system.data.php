a78cf5188f6fe8d2a002852a7f309b2415ba6601
Rachel L Carbone <rachellcarbone@gmail.com>
2016-02-29 20:03:11 -0500

    Missing DB prefix on table names.

--- api/v1/controllers/system/system.data.php
+++ api/v1/controllers/system/system.data.php
@@ -4,27 +4,27 @@
 class SystemData {
     
     static function deleteExpiredAuthTokens() {
-        return DBConn::delete("DELETE FROM as_tokens_auth WHERE expires < NOW();");
+        return DBConn::delete("DELETE FROM " . DBConn::prefix() . "tokens_auth WHERE expires < NOW();");
     }
     
     static function cleanLookupGroupRole() {
-        return DBConn::delete("DELETE as_auth_lookup_group_role FROM as_auth_lookup_group_role "
-                . "LEFT JOIN as_auth_groups AS g ON auth_group_id = g.id "
-                . "LEFT JOIN as_auth_roles AS r ON auth_role_id = r.id "
+        return DBConn::delete("DELETE " . DBConn::prefix() . "auth_lookup_group_role FROM " . DBConn::prefix() . "auth_lookup_group_role "
+                . "LEFT JOIN " . DBConn::prefix() . "auth_groups AS g ON auth_group_id = g.id "
+                . "LEFT JOIN " . DBConn::prefix() . "auth_roles AS r ON auth_role_id = r.id "
                 . "WHERE g.id IS NULL OR r.id IS NULL");
     }
     
     static function cleanLookupRoleField() {
-        return DBConn::delete("DELETE as_auth_lookup_role_field FROM as_auth_lookup_role_field "
-                . "LEFT JOIN as_auth_fields AS f ON auth_field_id = f.id "
-                . "LEFT JOIN as_auth_roles AS r ON auth_role_id = r.id "
+        return DBConn::delete("DELETE " . DBConn::prefix() . "auth_lookup_role_field FROM " . DBConn::prefix() . "auth_lookup_role_field "
+                . "LEFT JOIN " . DBConn::prefix() . "auth_fields AS f ON auth_field_id = f.id "
+                . "LEFT JOIN " . DBConn::prefix() . "auth_roles AS r ON auth_role_id = r.id "
                 . "WHERE f.id IS NULL OR r.id IS NULL");
     }
     
     static function cleanLookupUserGroup() {
-        return DBConn::delete("DELETE as_auth_lookup_user_group FROM as_auth_lookup_user_group "
-                . "LEFT JOIN as_auth_groups AS g ON auth_group_id = g.id "
-                . "LEFT JOIN as_auth_users AS u ON user_id = u.id "
+        return DBConn::delete("DELETE " . DBConn::prefix() . "auth_lookup_user_group FROM " . DBConn::prefix() . "auth_lookup_user_group "
+                . "LEFT JOIN " . DBConn::prefix() . "auth_groups AS g ON auth_group_id = g.id "
+                . "LEFT JOIN " . DBConn::prefix() . "auth_users AS u ON user_id = u.id "
                 . "WHERE g.id IS NULL OR u.id IS NULL");
     }
     
