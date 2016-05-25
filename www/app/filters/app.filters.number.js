9752ce1b141258448d6fe520e0e94bdedd02bd4d
Rachel L Carbone <rachellcarbone@gmail.com>
2016-05-25 12:14:34 -0400

    Round float numbers to two digits.

--- www/app/filters/app.filters.number.js
+++ www/app/filters/app.filters.number.js
@@ -31,3 +31,35 @@ app.filter("formatMySQLDate", function () {
         return moment(value, 'YYYY-MM-DD HH:mm:ss').tz('America/New_York').format('M/D/YYYY h:mm a');
     };
 });
+
+app.filter('numberEx', ['numberFilter', '$locale',
+    function (number, $locale) {
+
+        var formats = $locale.NUMBER_FORMATS;
+        return function (input, fractionSize) {
+            
+            if(!angular.isDefined(input)) {
+                return input;
+            }
+            
+            if(!angular.isDefined(fractionSize)) {
+                fractionSize = 2;
+            }
+            //Get formatted value
+            var formattedValue = number(input, fractionSize);
+
+            //get the decimalSepPosition
+            var decimalIdx = formattedValue.indexOf(formats.DECIMAL_SEP);
+
+            //If no decimal just return
+            if (decimalIdx === -1) {
+                return formattedValue;
+            } else {
+                var whole = formattedValue.substring(0, decimalIdx);
+                var decimal = (Number(formattedValue.substring(decimalIdx)) || "").toString();
+
+                return whole + decimal.substring(1);
+            }
+        };
+    }
+]);
