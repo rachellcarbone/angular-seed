15beefb8db02330d1634119ebf3bdcf18f524186
Rachel L Carbone <rachellcarbone@gmail.com>
2016-05-25 16:39:41 -0400

    More specific lists

--- api/v1/controllers/simple-lists/lists.data.php
+++ api/v1/controllers/simple-lists/lists.data.php
@@ -5,15 +5,17 @@ class ListsData {
     
     static function selectUsers() {
         return DBConn::selectAll("SELECT u.id, CONCAT(u.name_last, ', ', u.name_first, '   (ID: ', u.id, ')') AS label "
-                . "FROM " . DBConn::prefix() . "users AS u ORDER BY u.name_last;");
+                . "FROM " . DBConn::prefix() . "users AS u WHERE u.disabled IS NULL ORDER BY u.name_last;");
     }
     
     static function selectGroups() {
-        return DBConn::selectAll("SELECT g.id, g.group AS label FROM " . DBConn::prefix() . "auth_groups AS g ORDER BY g.group;");
+        return DBConn::selectAll("SELECT g.id, g.group AS label "
+                . "FROM " . DBConn::prefix() . "auth_groups AS g ORDER BY g.group;");
     }
     
     static function selectRoles() {
-        return DBConn::selectAll("SELECT r.id, r.role AS label FROM " . DBConn::prefix() . "auth_roles AS r ORDER BY r.role;");
+        return DBConn::selectAll("SELECT r.id, r.role AS label "
+                . "FROM " . DBConn::prefix() . "auth_roles AS r ORDER BY r.role;");
     }
     
     static function selectVisibilityFields() {
