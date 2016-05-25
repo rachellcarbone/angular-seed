bc2b55d1ec2782280abf7d2b42669fa3c444baa8
Rachel L Carbone <rachellcarbone@gmail.com>
2016-05-25 16:38:23 -0400

    Field Visibility API Updates

--- api/v1/controllers/field-visibility/fields.data.php
+++ api/v1/controllers/field-visibility/fields.data.php
@@ -28,15 +28,15 @@ class FieldData {
         $field = DBConn::selectOne("SELECT f.id, f.identifier, f.type, f.desc, f.initialized, f.created, f.last_updated AS lastUpdated, "
                 . "CONCAT(u1.name_first, ' ', u1.name_last) AS createdBy, CONCAT(u2.name_first, ' ', u2.name_last) AS updatedBy "
                 . "FROM " . DBConn::prefix() . "auth_fields AS f "
-                . "JOIN " . DBConn::prefix() . "users AS u1 ON u1.id = f.created_user_id "
-                . "JOIN " . DBConn::prefix() . "users AS u2 ON u2.id = f.last_updated_by "
-                . "WHERE f.identifier = :identifier AND f.id ! = :id "
+                . "LEFT JOIN " . DBConn::prefix() . "users AS u1 ON u1.id = f.created_user_id "
+                . "LEFT JOIN " . DBConn::prefix() . "users AS u2 ON u2.id = f.last_updated_by "
+                . "WHERE f.identifier = :identifier AND f.id != :id "
                 . "LIMIT 1;", array(':identifier' => $identifier, ':id' => $fieldId));
 
         $qRoles = DBConn::preparedQuery("SELECT r.id, r.role, r.desc "
                 . "FROM " . DBConn::prefix() . "auth_roles AS r "
                 . "JOIN " . DBConn::prefix() . "auth_lookup_role_field AS look ON r.id = look.auth_role_id "
-                . "WHERE look.auth_field_id = :id ORDER BY r.role;");
+                . "WHERE look.auth_field_id = :id GROUP BY r.id ORDER BY r.role;");
         
         if($field) {          
             $qRoles->execute(array(':id' => $field->id));
@@ -47,17 +47,17 @@ class FieldData {
     }
   
     static function insertField($validField) {
-        return DBConn::insert("INSERT INTO " . DBConn::prefix() . "auth_fields(`identifier`, `type`, `desc`, `created_user_id`, `last_updated_by`) "
+        return DBConn::insert("INSERT INTO " . DBConn::prefix() . "auth_fields(identifier, type, desc, created_user_id, last_updated_by) "
                 . "VALUES (:identifier, :type, :desc, :created_user_id, :last_updated_by)", $validField);
     }
     
     static function updateFieldInitialize($validField) {
-        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_fields SET `initialized`=:initialized WHERE `id`=:id;", $validField);
+        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_fields SET initialized=:initialized WHERE id=:id;", $validField);
     }
     
     static function updateField($validField) {
-        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_fields SET `identifier`=:identifier, `type`=:type, "
-                . "`desc`=:desc, `last_updated_by`=:last_updated_by, initialized=NULL WHERE `id`=:id;", $validField);
+        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_fields SET identifier=:identifier, type=:type, "
+                . "desc=:desc, last_updated_by=:last_updated_by, initialized=NULL WHERE id=:id;", $validField);
     }
     
     static function deleteField($id) {
@@ -68,12 +68,41 @@ class FieldData {
     }
   
     static function insertRoleAssignment($data) {
-        return DBConn::insert("INSERT INTO " . DBConn::prefix() . "auth_lookup_role_field(`auth_field_id`, `auth_role_id`, `created_user_id`) "
+        return DBConn::insert("INSERT INTO " . DBConn::prefix() . "auth_lookup_role_field(auth_field_id, auth_role_id, created_user_id) "
                 . "VALUES (:auth_field_id, :auth_role_id, :created_user_id)", $data);
     }
                     
     static function deleteRoleAssignment($data) {
         return DBConn::delete("DELETE FROM " . DBConn::prefix() . "auth_lookup_role_field "
                 . "WHERE auth_field_id = :auth_field_id AND auth_role_id = :auth_role_id;", $data);
+    }
+    
+    
+    /* Element Visibility */
+    
+    static function selectVisibilityKey() {
+        
+        $qElements = DBConn::executeQuery("SELECT id, identifier, initialized "
+                . "FROM " . DBConn::prefix() . "auth_fields WHERE initialized = 1 ORDER BY identifier;");
+        
+        $qRoles = DBConn::preparedQuery("SELECT auth_role_id AS id "
+                . "FROM " . DBConn::prefix() . "auth_lookup_role_field "
+                . "WHERE auth_field_id = :auth_field_id "
+                . "GROUP BY auth_role_id ORDER BY auth_role_id;");
+
+        $elements = Array();
+
+        while ($elem = $qElements->fetch(\PDO::FETCH_OBJ)) {
+            $qRoles->execute(array(':auth_field_id' => $elem->id));
+            $elem->roles = $qRoles->fetchAll(PDO::FETCH_COLUMN);
+            array_push($elements, $elem);
+        }
+        
+        return $elements;
+    }
+    
+    static function updateVisibilityElementInit($identifier) {
+        return DBConn::update("UPDATE " . DBConn::prefix() . "auth_fields SET initialized=NOW(), last_updated_by=:last_updated_by "
+                . "WHERE identifier=:identifier LIMIT 1;", $identifier);
     }
 }
