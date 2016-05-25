d6021814846c35219a344189de559433e4bf8fe3
Rachel L Carbone <rachellcarbone@gmail.com>
2016-05-25 13:32:47 -0400

    Modal Updates

---
+++ www/app/modals/auth/forgotPassword/forgotPassword.controller.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+angular.module('app.modal.forgotPassword', [])
+    .controller('ForgotPasswordCtrl', ['$scope', '$uibModalInstance', 'ForgotEmailAddress', 'AuthService',
+    function ($scope, $uibModalInstance, ForgotEmailAddress, AuthService) {
+
+        /* Used to restrict alert bars */
+        $scope.alertProxy = {};
+
+        /* Holds the form on the modal */
+        $scope.form = {};
+
+        $scope.forgotpasswordEmail = {
+            'email': ForgotEmailAddress
+        };
+
+        /* Click event for the Cancel button */
+        $scope.buttonCancel = function () {
+            $uibModalInstance.dismiss(false);
+        };
+
+        /* Click event for the Submit button*/
+        $scope.buttonSubmit = function () {
+
+            //Make API Call to send reset password link in mail
+            $scope.$broadcast('show-errors-check-validity');
+            if ($scope.form.modalForm.$valid) {
+                AuthService.forgotpassword($scope.forgotpasswordEmail).then(function (results) {
+                }, function (error) {
+                    $scope.alertProxy.error(error);
+                });
+            } else {
+                $scope.form.modalForm.$setDirty();
+                $scope.alertProxy.error('Please fill in both fields.');
+            }
+
+
+
+        };
+
+
+
+    }]);
---
+++ www/app/modals/auth/signupInvite/signupInvite.controller.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/* @author  Rachel Carbone */
+
+angular.module('app.modal.signupInvite', [])        
+    .controller('SignupInviteModalCtrl', ['$scope', '$uibModalInstance', 'ApiRoutesEmails', 'InvitingPlayer',
+    function($scope, $uibModalInstance, ApiRoutesEmails, InvitingPlayer) {   
+    
+    $scope.invitingPlayer = InvitingPlayer;
+    $scope.invitingTeamName = (InvitingPlayer && angular.isDefined(InvitingPlayer.teams[0].name)) ? InvitingPlayer.teams[0].name : '';
+    
+    /* Used to restrict alert bars */
+    $scope.alertProxy = {};
+    
+    /* Holds the add / edit form on the modal */
+    $scope.form = {};
+    
+    /* Item to display and edit */
+    $scope.invite = {
+        'email' : '',
+        'phone' : '',
+        'nameFirst' : '',
+        'nameLast' : ''
+    };
+    
+    /* Click event for the Add Team button */
+    $scope.buttonInvite = function() {
+        if($scope.invitingPlayer && $scope.invitingPlayer.teams[0].id) {
+            
+            $scope.invite.invitedById = $scope.invitingPlayer.id;
+            $scope.invite.invitedByFirstName = $scope.invitingPlayer.nameFirst;
+            $scope.invite.invitedByLastName = $scope.invitingPlayer.nameLast;
+            $scope.invite.teamId = $scope.invitingPlayer.teams[0].id;
+            $scope.invite.teamName = $scope.invitingPlayer.teams[0].name;
+            
+            ApiRoutesEmails.sendTeamInviteEmail($scope.invite).then(function(response) {
+                $uibModalInstance.close(response.msg);
+            }, function(error) {
+                $scope.alertProxy.error(error);
+            });
+        } else {
+            ApiRoutesEmails.sendInviteNewPlayerEmail($scope.invite).then(function(response) {
+                $uibModalInstance.close(response.msg);
+            }, function(error) {
+                $scope.alertProxy.error(error);
+            });
+        }
+    };
+        
+    /* Click event for the Cancel button */
+    $scope.buttonCancel = function() {
+        $uibModalInstance.dismiss(false);
+    };
+    
+}]);
