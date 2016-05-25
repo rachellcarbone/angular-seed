397431654011047fb4cc0abcccb91c3545866e55
Rachel L Carbone <rachellcarbone@gmail.com>
2016-05-25 12:48:48 -0400

    Add body class to page.

--- www/app/config/app.run.js
+++ www/app/config/app.run.js
@@ -17,6 +17,9 @@ app.run(function ($rootScope) {
         // If the new state has property for 'title' (set in the state declaration) 
         // set the root scope variable title to be used by the index.html template.
         $rootScope.title = (current.hasOwnProperty('title')) ? current.title : 'Rachel\'s New Project Seed';
+        // If the new state has property for 'bodyClass' (set in the state declaration) 
+        // set the root scope variable title to be used by the index.html template.
+        $rootScope.bodyClass = (current.hasOwnProperty('bodyClass')) ? current.bodyClass : '';
     });
     
 });
